c365a60c91c548b05d1ebef115ba91431b3c7936
furbox <furbox@gmail.com>
2021-08-15 14:02:34 -0500

    fastify starter

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ fastify.get('/', function (request, reply) {
 // Run the server!
 fastify.listen(3000, function (err, address) {
     if (err) {
-        fastify.log.error(err)
-        process.exit(1)
+        fastify.log.error(err);
+        process.exit(1);
     }
-    fastify.log.info(`server listening on ${address}`)
+    fastify.log.info(`server listening on ${address}`);
 })
